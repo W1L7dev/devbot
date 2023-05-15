352063c24e75ec568eeeb499a6cdaf49777666f6
W1L7dev <williammiclette@icloud.com>
2023-05-15 19:43:54 -0400

    🚨 Removed web folder from master branch

--- web/main.js
+++
@@ -1,8 +0,0 @@
-function handleResponsiveNav() {
-  var x = document.getElementById("topnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}

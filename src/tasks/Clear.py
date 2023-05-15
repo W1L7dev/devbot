9a2e3237055dbc9af1245685668db275e526da7e
W1L7dev <williammiclette@icloud.com>
2023-05-15 19:36:54 -0400

    💻Website Branch

--- src/tasks/Clear.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def cls():
-    """Clears the console"""
-    if os.name != "nt":
-        os.system("clear")  # For UNIX systems (Linux, MacOS)
-    else:
-        os.system("cls")  # For Windows systems
--- src/tasks/Restart.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-
-def restart():
-    """Restarts the current script"""
-    if os.name == "nt":
-        os.system("cls")
-    os.execv(sys.executable, ["python"] + sys.argv)
